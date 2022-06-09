c3c0b97b038e989969c67a876b0b5c6af932c128
doneela <doneeladas@gmail.com>
2022-06-09 21:02:36 +0530

    Added Backend for Signup

---
+++ server/controller/user-controller.js
@@ -0,0 +1,11 @@
+
+
+
+
+export const userSignup = (request, response) =>{
+    try{
+        const user = request.body;
+    }catch(error){
+
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser'
 
 
 import Connection from './database/db.js';
 import DefaultData from './default.js';
+import Router from './routes/route.js';
+import { trusted } from 'mongoose';
 
 const app = express();
 
 dotenv.config();
 
-const PORT =8080;
+app.use(cors());
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/',Router);
+
+const PORT = 8080;
 
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 Connection(USERNAME, PASSWORD);
 
-app.listen(PORT, ()=> console.log(`server is running successfully on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server is running successfully on PORT ${PORT}`));
 
 DefaultData();
